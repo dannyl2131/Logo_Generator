13a6aa5111e03d5fed8904ea796417a9aba4674c
DLindquist <dannyl2131@gmail.com>
2023-05-01 22:56:11 -0600

    Completed logic and added examples and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Logo_Generator
+#Logo Generator
+  <br /> [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  <br /> ##Description
+  <br /> This is a logo generator for your projects. It creates a simple shape with 3 letters inside with colors of your choice. 
+  <br /> ##Table of Contents
+  <br /> 
+  <br /> ##Installation
+  <br /> In order to install, simply clone the repository on to your machine and run node index.js in the terminal to begin. Answer the questions you are presented with and a logo will be made and placed in to the examples folder for your use
+  <br /> ##Usage Information
+  <br /> 
+  <br /> ##Contributing
+  <br /> 
+  <br /> ##Questions
+  <br /> If there are any additional questions, please email me at dannyl2131@gmail.com
+
+  <br /> Here is a link to my Github profile if you'd like to check out any more of my work![github.com/dannyl2131](github.com/dannyl2131)
+  <br /> 
+    ##License
+    https://opensource.org/license/mit/      
--- index.js
+++ index.js
@@ -1,7 +1,64 @@
-const inquirer = require("inquirer")
-const test = require("jest")
+const inquirer = require("inquirer");
+const test = require("jest");
+const fs = require("fs");
+const {Shape, Triangle, Square, Circle} = require("./lib/shapes");
+const init = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "letters",
+        message: "What 3 characters would you like in your logo?",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "What color would you like your text to be?",
+      },
+      {
+        type: "list",
+        name: "shapes",
+        message: "What shape would you like your logo to be?",
+        choices: ["Triangle", "Square", "Circle"],
+      },
+      {
+        type: "input",
+        name: "fillColor",
+        message: "What color would you like your shape to be?",
+      },
+    ])
+    .then((data) => {
+      const shape = data.shapes;
+      if (shape == "Triangle") {
+        const triangle = new Triangle(
+          data.fillColor,
+          data.letters,
+          data.textColor
+        );
+        console.log(triangle);
+        // const svg = triangle.render();
+        // console.log(svg);
+        fs.writeFile("examples/triangle.svg", triangle.render(), (err) => {
+          err ? console.log(err) : console.log("success");
+        });
+      } else if (shape == "Square") {
+        const square = new Square(data.fillColor, data.letters, data.textColor);
+        console.log(square);
+        // const svg = square.render();
+        // console.log(svg);
+        fs.writeFile("examples/Square.svg", square.render(), (err) => {
+          err ? console.log(err) : console.log("success");
+        });
+      } else {
+        const circle = new Circle(data.fillColor, data.letters, data.textColor);
+        console.log(circle);
+        // const svg = circle.render();
+        // console.log(svg);
+        fs.writeFile("examples/Circle.svg", circle.render(), (err) => {
+          err ? console.log(err) : console.log("success");
+        });
+      }
+    });
+};
 
-
-const init = function(){
-    console.log("Started app")
-}
+init();
